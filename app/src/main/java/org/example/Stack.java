7420e5e407c8c8de926fcaacb2ccdcf9ab718b9b
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2025-11-17 20:33:20 -0600

    initial

---
+++ app/src/main/java/org/example/Stack.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack {
+
+    List<T> data;
+
+    int size;
+
+    Stack () {}
+
+    Stack (int size) {
+
+        data = new List<T>;
+
+    }
+
+    // push
+    public void push() {}
+
+    // pop
+    public void pop() {}
+    // peek
+    public <T> T peek() {return;}
+    // size
+    public int size() {return ;}
+
+
+}
