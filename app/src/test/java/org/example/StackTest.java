f6301d70e67615ab4163c73d8b7f1b99cb284b9c
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2025-11-25 13:33:41 -0600

    update

--- app/src/test/java/org/example/StackTest.java
+++ app/src/test/java/org/example/StackTest.java
@@ -1,14 +1,42 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StackTest {
 
 
+    @Test
+    public void testStack() {
 
+        Stack <Dish> stack = new Stack(4);
 
+        Dish oneDish = new Dish("A dish with one fish pattern on it");
+        Dish twoDish = new Dish("A dish with two fish patterns on it");
+        Dish redDish = new Dish("A dish with a red fish pattern on it");
+        Dish blueDish = new Dish("A dish with a blue fish pattern on it");
 
+        stack.push(oneDish);
+        stack.push(twoDish);
+        stack.push(redDish);
+        stack.push(blueDish);
 
+        assertEquals(stack.size(),4);
 
+        assertEquals(stack.peek(),blueDish);
 
+        stack.pop();
 
+        assertEquals(stack.peek(),redDish);
+
+        assertEquals(stack.size(),3);
+
+        assertEquals(stack.peek(),redDish);
+
+        assertEquals(stack.isEmpty(),false);
+
+    }
 
 }
